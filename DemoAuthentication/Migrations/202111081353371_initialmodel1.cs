a994b4b9e773bf52b9d92a15178f68fed71f6a1f
Lvn160701 <leevannhat1607@gmail.com>
2021-11-15 09:07:09 +0700

    demoauthentication

---
+++ DemoAuthentication/Migrations/202111081353371_initialmodel1.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoAuthentication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialmodel1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FullName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "FullName");
+        }
+    }
+}
---
+++ DemoAuthentication/Models/RoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAuthentication.Models
+{
+    public class RoleViewModel
+    {
+        public RoleViewModel() { }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public RoleViewModel(ApplicationRole role)
+        {
+            Id = role.Id;
+            Name = role.Name;
+        }
+    }
+}
